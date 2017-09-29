50492755d71be83dcc882accea7213b228933d7e
cezary-draus <cezary.draus@gmail.com>
2017-09-29 22:38:29 +0200

    scale up rndity sponsor, was smaller than other platinum sponsors

--- docpad.js
+++ docpad.js
@@ -60,7 +60,7 @@ docpadConfig = {
                     },
                     {
                         "name": "Rndity",
-                        "logo_url": "/images/sponsors/rndity.png",
+                        "logo_url": "/images/sponsors/rndity_2.png",
                         "link": "http://rndity.com/"
                     },
                     {"name": "Seqr", "logo_url": "/images/sponsors/seqr.png", "link": "https://www.seqr.com/int/"},
